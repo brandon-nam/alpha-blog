715473cf4fcfd46881c49eedbce17389832649a6
brandon-nam <brandonnam2020@gmail.com>
2020-12-26 18:43:04 +0800

    added new article feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,17 @@ class ArticlesController < ApplicationController
 	def show 
 		@article = Article.find(params[:id])
 	end 
+	def new
+		@article = Article.new 
+	end 
+
+	def create
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		if @article.save
+			flash[:notice] = "Article was created successfully."
+			redirect_to @article #article_path(@article)
+		else 
+			render 'new'
+		end 
+	end 
 end 
